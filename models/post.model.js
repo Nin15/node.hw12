be6cc2ccd2bb6e0df9759baf94e31c378d3988f1
Nino Shubitidze <132759949+Nin15@users.noreply.github.com>
2025-12-23 23:35:56 +0400

    date format

--- models/post.model.js
+++ models/post.model.js
@@ -1,5 +1,7 @@
 const { default: mongoose } = require("mongoose");
+import dayjs from "dayjs";
 const now = new Date(Date.now());
+const formatted = dayjs(now).format("DD/MM/YYYY");
 
 const postSchema = new mongoose.Schema(
   {
@@ -18,8 +20,9 @@ const postSchema = new mongoose.Schema(
       likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
       dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
     },
-    date: { type: String, default: now.toLocaleString() },
+    date: { type: String, default: formatted, require: true },
   },
+
   { timestamps: true }
 );
 
