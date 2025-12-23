c6162c98b624512d4f5f31b8ca614eaed877b431
Nino Shubitidze <132759949+Nin15@users.noreply.github.com>
2025-12-23 23:39:56 +0400

    c

--- models/post.model.js
+++ models/post.model.js
@@ -1,7 +1,8 @@
 const { default: mongoose } = require("mongoose");
 import dayjs from "dayjs";
-const now = new Date(Date.now());
-const formatted = dayjs(now).format("DD/MM/YYYY");
+
+const formatted = dayjs(Date.now()).format('DD/MM/YYYY');
+
 
 const postSchema = new mongoose.Schema(
   {
